1b0b178a11979677398e1aed7b118f7029b36d5c
Santiago Caprile <santiagocaprile7@gmail.com>
2022-08-08 15:15:00 -0300

    Se acomodaron el nombre de las paginas y archivos vinculados

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const btnJugar = document.getElementById("btn-jugar");
 
     btnJugar.onclick = function() {
-        location.href = "./index.html";
+        location.href = "./tablero.html";
     }
 }
 
