77ae4caa6e4fbf64897f02c4d716971ce574e213
Santiago Caprile <santiagocaprile7@gmail.com>
2022-08-09 22:21:34 -0300

    Ahora si la lista de partidas está vacia no se carga la tabla y se muestra el mensaje

--- js/partidas.js
+++ js/partidas.js
@@ -5,28 +5,32 @@ window.onload = function() {
 
     if (partidas) {
         partidas = JSON.parse(partidas);
-        var tiempo = new Date();
-        for (var i = 0; i < partidas.length; i++) {
-            if(!partidas[i].ganada){ //lista solo las partidas no ganadas
-                var fila = document.createElement("tr");
-                var celdaNombre = document.createElement("td");
-                celdaNombre.innerHTML = partidas[i].nombre;
-                tiempo = new Date(partidas[i].tiempo);
-                var celdaTiempo = document.createElement("td");
-                celdaTiempo.innerHTML = (tiempo.getMinutes()<10? "0" + tiempo.getMinutes() : tiempo.getMinutes()) 
-                + ":" + (tiempo.getSeconds()<10? "0" + tiempo.getSeconds() : tiempo.getSeconds());
-                var celdaFlecha = document.createElement("td");
-                celdaFlecha.innerHTML = `<button class="botones-cargar" id="${i}">&rightarrowtail;</button>`;
-                fila.appendChild(celdaNombre);
-                fila.appendChild(celdaTiempo);
-                fila.appendChild(celdaFlecha);
-                tabla.appendChild(fila);
-                document.getElementById(i).onclick = function() {
-                    localStorage.setItem("partida", this.id);
-                    localStorage.setItem("nombre", partidas[this.id].nombre);
-                    location.href = "./tablero.html";
+        if(partidas.length > 0) {
+            var tiempo = new Date();
+            for (var i = 0; i < partidas.length; i++) {
+                if(!partidas[i].ganada){ //lista solo las partidas no ganadas
+                    var fila = document.createElement("tr");
+                    var celdaNombre = document.createElement("td");
+                    celdaNombre.innerHTML = partidas[i].nombre;
+                    tiempo = new Date(partidas[i].tiempo);
+                    var celdaTiempo = document.createElement("td");
+                    celdaTiempo.innerHTML = (tiempo.getMinutes()<10? "0" + tiempo.getMinutes() : tiempo.getMinutes()) 
+                    + ":" + (tiempo.getSeconds()<10? "0" + tiempo.getSeconds() : tiempo.getSeconds());
+                    var celdaFlecha = document.createElement("td");
+                    celdaFlecha.innerHTML = `<button class="botones-cargar" id="${i}">&rightarrowtail;</button>`;
+                    fila.appendChild(celdaNombre);
+                    fila.appendChild(celdaTiempo);
+                    fila.appendChild(celdaFlecha);
+                    tabla.appendChild(fila);
+                    document.getElementById(i).onclick = function() {
+                        localStorage.setItem("partida", this.id);
+                        localStorage.setItem("nombre", partidas[this.id].nombre);
+                        location.href = "./tablero.html";
+                    }
                 }
             }
+        } else {
+            tabla.innerHTML = "<td>No hay partidas guardadas</td>";
         }
     } else {
         tabla.innerHTML = "<td>No hay partidas guardadas</td>";
