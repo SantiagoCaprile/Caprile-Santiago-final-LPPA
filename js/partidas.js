484ad2f0b79ea5fec260f25c0ff7b8b1e9a9afb1
Santiago Caprile <santiagocaprile7@gmail.com>
2022-08-10 19:00:05 -0300

    Funcionalidad a los botones de ordenar agregada

--- js/partidas.js
+++ js/partidas.js
@@ -4,33 +4,38 @@ window.onload = function() {
     const tbody = document.getElementById("lista-ganadas");
     const btnGanadas = document.getElementById("btn-ganadas");
     const btnVolver = document.getElementById("btn-volver");
+    const btnTiempo = document.getElementById("ordena-tiempo");
+    const btnFecha = document.getElementById("ordena-fecha");
     var partidas = localStorage.getItem("partidas");
-    var hayListadas = false;
     if (partidas) {
         partidas = JSON.parse(partidas);
-        if(partidas.length > 0) {
-            var tiempo = new Date();
-            for (var i = 0; i < partidas.length; i++) {
-                if(!partidas[i].ganada){ //lista solo las partidas no ganadas
-                    var fila = document.createElement("tr");
-                    var celdaNombre = document.createElement("td");
-                    celdaNombre.innerHTML = partidas[i].nombre;
-                    tiempo = new Date(partidas[i].tiempo);
-                    var celdaTiempo = document.createElement("td");
-                    celdaTiempo.innerHTML = (tiempo.getMinutes()<10? "0" + tiempo.getMinutes() : tiempo.getMinutes())
-                    + ":" + (tiempo.getSeconds()<10? "0" + tiempo.getSeconds() : tiempo.getSeconds());
-                    var celdaFlecha = document.createElement("td");
-                    celdaFlecha.innerHTML = `<button class="botones-cargar" id="${i}">&rightarrowtail;</button>`;
-                    fila.appendChild(celdaNombre);
-                    fila.appendChild(celdaTiempo);
-                    fila.appendChild(celdaFlecha);
-                    tabla.appendChild(fila);
-                    hayListadas = true;
-                    document.getElementById(i).onclick = function() {
-                        localStorage.setItem("partida", this.id);
-                        localStorage.setItem("nombre", partidas[this.id].nombre);
-                        location.href = "./tablero.html";
-                    }
+    }
+    var ganadas = partidas.filter(function(partida) {
+        return partida.ganada;
+    });
+    var hayListadas = false;
+    if(partidas.length > 0) {
+        var tiempo = new Date();
+        for (var i = 0; i < partidas.length; i++) {
+            if(!partidas[i].ganada){ //lista solo las partidas no ganadas
+                var fila = document.createElement("tr");
+                var celdaNombre = document.createElement("td");
+                celdaNombre.innerHTML = partidas[i].nombre;
+                tiempo = new Date(partidas[i].tiempo);
+                var celdaTiempo = document.createElement("td");
+                celdaTiempo.innerHTML = (tiempo.getMinutes()<10? "0" + tiempo.getMinutes() : tiempo.getMinutes())
+                + ":" + (tiempo.getSeconds()<10? "0" + tiempo.getSeconds() : tiempo.getSeconds());
+                var celdaFlecha = document.createElement("td");
+                celdaFlecha.innerHTML = `<button class="botones-cargar" id="${i}">&rightarrowtail;</button>`;
+                fila.appendChild(celdaNombre);
+                fila.appendChild(celdaTiempo);
+                fila.appendChild(celdaFlecha);
+                tabla.appendChild(fila);
+                hayListadas = true;
+                document.getElementById(i).onclick = function() {
+                    localStorage.setItem("partida", this.id);
+                    localStorage.setItem("nombre", partidas[this.id].nombre);
+                    location.href = "./tablero.html";
                 }
             }
         }
@@ -40,9 +45,6 @@ window.onload = function() {
     }
 
     cargarTablaGanadas = function() {
-        var ganadas = partidas.filter(function(partida) {
-            return partida.ganada;
-        });
         if(ganadas.length > 0) {
             for (var i = 0; i < ganadas.length; i++) {
                 var fila = document.createElement("tr");
@@ -83,4 +85,43 @@ window.onload = function() {
         e.preventDefault();
         document.getElementById("modal-ganadas").classList.add("oculto");
     }
+
+    //ordena segun funcion que se le pase
+    ordenaGanadas = function(funcion) {
+        if(tbody.hasChildNodes()) {
+            tbody.innerHTML = "";
+        }
+        ganadas.sort(funcion);
+    }
+
+    var contadorFecha = 0;
+    var contadorTiempo = 0;
+    btnFecha.onclick = function(e) {
+        e.preventDefault();
+        ordenaGanadas(function(a, b) {
+            return new Date(a.tiempo) - new Date(b.tiempo);
+        } );
+        if(contadorFecha%2 == 0) {
+            ganadas.reverse();
+        }
+        contadorFecha++;
+        cargarTablaGanadas();
+    }
+
+    btnTiempo.onclick = function(e) {
+        e.preventDefault();
+        ordenaGanadas(function(a, b) {
+            var t1 = new Date(a.tiempo);
+            var t2 = new Date(b.tiempo);
+            t1 = t1.getMinutes() * 60 + t1.getSeconds();
+            t2 = t2.getMinutes() * 60 + t2.getSeconds();
+            return t1 - t2;
+        } );
+        if(contadorTiempo%2) {
+            ganadas.reverse();
+        }
+        contadorTiempo++;
+        cargarTablaGanadas();
+    }
+
 }
